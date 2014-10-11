03d13c0dca06b5492c84a901b19ef5c145804a4b
Gene Cho <genec2008@gmail.com>
2014-10-11 09:29:18 -0700

    Debugging Plot_Waveform()

--- problem_set1/problem_set1.py
+++ problem_set1/problem_set1.py
@@ -172,26 +172,59 @@ def plot_waveforms(time,voltage,APTimes,titlestr):
    
     plt.figure()
    
-    ## Your Code Here 
-    
-    # find index of action potential in t array
-    APindex = plt.find(t == APTimes[1])
-    
-    # calculate number of measurements on each side of AP
-    sideWidth = 0.003
-    sideBins = int(sideWidth / (t[1]-t[0]))
-    # obtain list of indices for AP measurements within +/- sideWidth
-    indices = range(APindex - sideBins, APindex + sideBins)
-    
-    tempT = t[indices]
-    tempV = v[indices]
-    
-    i=0
-    temp[] = len(v)
-    while t[APindex + i] >= APTimes[1] - 0.003:
-        temp[i] = v[APindex + i]
+    # calculate number of measurements ("bins") to capture for each AP
+    sideWidth = 0.003   # +/- 3 milliseconds
+    timeStep = t[1]-t[0]
+    sideBins = int(sideWidth / timeStep)
+    bins = sideBins * 2
+        
+    # re-scale x-axis (time) with AP spike centered at zero
+#    subsetT = np.ones(sideBins*2)
+#    subsetT[0] = -sideWidth
+#    for i in range(0, len(subsetT)):
+#       subsetT[i] = subsetT[0] + i*timeStep   
    
-    plt.show()
+    # alternate way
+    subsetT = np.linspace(-sideWidth, sideWidth, bins)  
+    subsetV = np.zeros(bins)
+   
+    for val in APTimes:     
+        # find index of action potential (AP) in t data array
+        tIndex = plt.find(t == APTimes[val])
+        
+        ## obtain indices for the data subset near a given AP        
+        startIndex = tIndex - sideBins
+        if startIndex < 0:  # make sure index is never less than zero
+            startIndex = 0    
+        endIndex = tIndex + sideBins
+        indices = range(startIndex, endIndex)
+    
+        # subset dataset around given AP
+        # Warning:  if AP too close to zero the dataset is too small
+        # and needs to be zero-filled
+        if len(indices) == bins:   # complete data available
+            # subsetT = t[indices]
+            subsetV = v[indices]
+        else:                       # close to zero, special handling
+            missing = bins - len(indices)
+            for i in range(missing):
+                subsetV[i] = 0      # zero-fill unavailable values         
+            i = missing
+            j = 1
+            while i < bins:         # get actual voltage measurement
+                subsetV[i] = v[indices[j]]               
+        # plot waveform of zoomed in view of action potential
+        print('Plotting waveform', subsetT[0], subsetT[-1],
+              subsetV[0], subsetV[-1]) 
+        
+        plt.plot(subsetT, subsetV, hold=True)
+        plt.show()
+
+    # Make and label plot  
+    plt.xlabel('Time (s)')
+    plt.ylabel('Voltage (uV)')
+    plt.title(titlestr)               
+    #plt.show()
     
 
         
@@ -208,7 +241,7 @@ def plot_waveforms(time,voltage,APTimes,titlestr):
 t,v = load_data('spikes_example.npy')
 APTimes = bad_AP_finder(t,v)
 plot_spikes(t,v,APTimes,'Action Potentials in Raw Signal')
-
+plot_waveforms(t,v,APTimes,'Waveforms')
 
 
 
